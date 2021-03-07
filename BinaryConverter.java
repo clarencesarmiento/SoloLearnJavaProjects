22009ad39df67cd1bfea6cc575192f225d1a50fe
Zankii <60923896+ClarenceSarmiento@users.noreply.github.com>
2021-03-07 20:16:17 +0800

    Create BinaryConverter.java

---
+++ BinaryConverter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+class Converter {
+    public static String toBinary(int num) {
+    String binary = "";
+    while (num > 0){
+        binary = (num % 2) + binary;
+        num /= 2;
+        }
+        return binary;
+    }
+}
+
+public class BinaryConverter {
+    public static void main(String[ ] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.print(Converter.toBinary(x));
+    }
+}
